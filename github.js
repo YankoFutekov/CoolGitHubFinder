57a081505cf59001aaed08a6a5229fc2150e6f69
Yanko <YankoFutekov@gmail.com>
2018-03-14 16:48:21 +0200

    Initial commit

---
+++ github.js
@@ -0,0 +1,22 @@
+class Github {
+  constructor() {
+    this.client_id = '56cb740fa45729a8745c';
+    this.client_secret = '275b2a16b84c45280eb45ca1f7e7b58c8e0f4f8d';
+    this.repos_count = 10;
+    this.repos_sort = 'created: asc';
+  }
+
+  async getUser(user) {
+    const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const repoResponse = await fetch(`https://api.github.com/users/${user}/repos?per_page=${this.repos_count}&sort=${this.repos_sort}&client_id=${this.client_id}&client_secret=${this.client_secret}`);
+
+    const profile = await profileResponse.json();
+    const repos = await repoResponse.json();
+
+    return {
+      profile,
+      repos
+    }
+  }
+}
